4dc72fa8820b8b531b2edd63470d17ed09071022
fsietsma <fsietsma@uva.nl>
2013-09-25 17:23:39 +0200

    Implemented BM25

--- src/python/ranker/model/BM25.py
+++ src/python/ranker/model/BM25.py
@@ -13,6 +13,27 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Lerot.  If not, see <http://www.gnu.org/licenses/>.
 
-from Linear import Linear
-from OneHiddenLayer import OneHiddenLayer
-from BM25 import BM25
+import numpy as np
+from AbstractRankingModel import AbstractRankingModel
+
+
+class BM25(AbstractRankingModel):
+
+    def __init__(self, feature_count):
+        self.feature_count = 3
+
+    def initialize_weights(self, init_method):
+        return np.array([2.5, 0, 0.8])
+
+    def score(self, features, w):
+        k1, k3, b = w
+        s = 0.0
+        nr_terms = len(features)/4
+        for i in range(nr_terms):
+            idf = features[i*4]
+            tf = features[i*4+1]
+            qtf = features[i*4+2]
+            dl = features[i*4+3]
+            s += ((idf * tf * (k1 + 1)) / (tf + k1 * (1-b+b*dl))) * (((k3+1)*qtf)/(k3+qtf)) 
+        return s
+    
