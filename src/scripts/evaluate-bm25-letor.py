8666af3b6120f02f50359d1994f407ee72af4966
fsietsma <fsietsma@uva.nl>
2013-09-26 16:00:12 +0200

    Fixed a bug

--- src/scripts/evaluate-bm25-letor.py
+++ src/scripts/evaluate-bm25-letor.py
@@ -3,11 +3,11 @@ import include
 import evaluation, query, ranker
 import numpy as np
 from ranker.AbstractRankingFunction import AbstractRankingFunction
+w = [0]*64
+w[int(sys.argv[2])] = 1
+wstr = ",".join([str(x) for x in w])
 
 evaluation = evaluation.NdcgEval()
-bm25ranker = AbstractRankingFunction(["ranker.model.Linear"], 'first', 64, sample="utils.sample_fixed")
-w = [0]*64
-w[24] = 1
-bm25ranker.update_weights(np.array(w))
+bm25ranker = AbstractRankingFunction(["ranker.model.Linear"], 'first', 64, init=wstr, sample="utils.sample_fixed")
 queries = query.load_queries(sys.argv[1], 64)
 print evaluation.evaluate_all(bm25ranker, queries)
