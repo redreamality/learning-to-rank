55b97f629abf8308ef935f39f322ac3a8efac6c7
fsietsma <fsietsma@uva.nl>
2013-09-26 15:47:49 +0200

    Switched document names

--- src/scripts/evaluate-bm25.py
+++ src/scripts/evaluate-bm25.py
@@ -1,13 +1,14 @@
 import sys
 import include
 import evaluation, query, ranker
-import numpy as np
 from ranker.AbstractRankingFunction import AbstractRankingFunction
 
 evaluation = evaluation.NdcgEval()
-bm25ranker = AbstractRankingFunction(["ranker.model.Linear"], 'first', 64, sample="utils.sample_fixed")
-w = [0]*64
-w[24] = 1
-bm25ranker.update_weights(np.array(w))
+bm25ranker = AbstractRankingFunction(["ranker.model.BM25"], 'first', 3, sample="utils.sample_fixed")
 queries = query.load_queries(sys.argv[1], 64)
 print evaluation.evaluate_all(bm25ranker, queries)
+
+# for k1 in np.arange(1.5, 3.5, 0.2):
+#     for b in np.arange(0, 1.6, 0.2):
+#         bm25ranker.updateweights(np.array([k1,0,b]))
+#         print "k1 = "+k1+" b = " + b + " score = " + evaluation.evaluate_all(bm25ranker, queries)
