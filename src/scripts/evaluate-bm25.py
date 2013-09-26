99cfda7dd0d2fea62a5f7cf57414de3e2e635e0c
fsietsma <fsietsma@uva.nl>
2013-09-26 18:06:11 +0200

    fixed BM25 (to handle multiple docs) implemented parameter sweep

--- src/scripts/evaluate-bm25.py
+++ src/scripts/evaluate-bm25.py
@@ -1,14 +1,21 @@
 import sys
 import include
 import evaluation, query, ranker
+import numpy as np
 from ranker.AbstractRankingFunction import AbstractRankingFunction
 
 evaluation = evaluation.NdcgEval()
 bm25ranker = AbstractRankingFunction(["ranker.model.BM25"], 'first', 3, sample="utils.sample_fixed")
 queries = query.load_queries(sys.argv[1], 64)
-print evaluation.evaluate_all(bm25ranker, queries)
+#print evaluation.evaluate_all(bm25ranker, queries)
 
-# for k1 in np.arange(1.5, 3.5, 0.2):
-#     for b in np.arange(0, 1.6, 0.2):
-#         bm25ranker.updateweights(np.array([k1,0,b]))
-#         print "k1 = "+k1+" b = " + b + " score = " + evaluation.evaluate_all(bm25ranker, queries)
+itt = int(sys.argv[2])
+fh = open(sys.argv[1] + ".out."+str(itt)+".txt", "w")
+
+
+for k1 in np.arange(1, 4, 0.1):
+     for b in np.arange(0.4, 1.2, 0.05):
+         for k3 in np.arange(100*itt, 100*(itt+1), 10):
+             bm25ranker.update_weights(np.array([k1,k3,b]))
+             print >> fh, "k1:%f k3:%f b:%f score:%f" % (k1, k3, b, evaluation.evaluate_all(bm25ranker, queries))
+fh.close()
