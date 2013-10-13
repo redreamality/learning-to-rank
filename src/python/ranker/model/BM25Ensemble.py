cf37e419724c267fa332e57e0d297e0c6259dfb0
Anne Schuth <anne.schuth@gmail.com>
2013-10-14 00:00:07 +0200

    bugfix

--- src/python/ranker/model/BM25Ensemble.py
+++ src/python/ranker/model/BM25Ensemble.py
@@ -21,6 +21,7 @@ from AbstractRankingModel import AbstractRankingModel
 class BM25Ensemble(AbstractRankingModel):
 
     def __init__(self, feature_count):
+        self.originalcount = feature_count
         self.feature_count = feature_count + 3
 
     def initialize_weights(self, init_method):
@@ -34,23 +35,22 @@ class BM25Ensemble(AbstractRankingModel):
     def scoreBM25(self, features, w):
         k1, k3, b = w
         s = 0.0
-        nr_terms = len(docfeatures) / 4
+        nr_terms = len(features) / 4
         for i in range(nr_terms):
-            idf = docfeatures[i * 4]
-            tf = docfeatures[i * 4 + 1]
-            qtf = docfeatures[i * 4 + 2]
-            dl = docfeatures[i * 4 + 3]
+            idf = features[i * 4]
+            tf = features[i * 4 + 1]
+            qtf = features[i * 4 + 2]
+            dl = features[i * 4 + 3]
             if dl == 0: continue
-            s += ((idf * tf * (k1 + 1)) / (tf + k1 * (1-b+b*dl))) *
-             (((k3+1)*qtf)/(k3+qtf))
+            s += ((idf * tf * (k1 + 1)) / (tf + k1 * (1-b+b*dl))) * (((k3+1)*qtf)/(k3+qtf))
         return s
 
     def score(self, features, w):
         scores = []
         for docfeatures in features:
-            bm25s = self.scoreBM25(docfeatures, w[-3:])
+            bm25s = self.scoreBM25(docfeatures[self.originalcount:], w[-3:])
             docfeatures[24] = bm25s
-            s = self.scoreLinear(docfeatures, w[:-3])
+            s = self.scoreLinear(docfeatures[:self.originalcount], w[:-3])
             scores.append(s)
         return scores
 
