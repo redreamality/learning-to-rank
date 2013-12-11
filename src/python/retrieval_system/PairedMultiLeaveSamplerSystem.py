d52d0a72d703f2408b2c007da9b780c2471b0c32
Anne Schuth <anne.schuth@gmail.com>
2013-12-11 22:32:57 +0100

    added pairedmultileave

---
+++ src/python/retrieval_system/PairedMultiLeaveSamplerSystem.py
@@ -0,0 +1,40 @@
+# This file is part of Lerot.
+#
+# Lerot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Lerot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Lerot.  If not, see <http://www.gnu.org/licenses/>.
+
+from BaselineSamplerSystem import BaselineSamplerSystem
+
+
+class PairedMultiLeaveSamplerSystem(BaselineSamplerSystem):
+    def get_ranked_list(self, query):
+        self.r1, self.r2 = self.sampler.get_arms()
+        (l, context) = self.comparison.interleave([self.r1, self.r2],
+                                                  query,
+                                                  self.nr_results)
+        self.current_l = l
+        self.current_context = context
+        self.current_query = query
+        return l
+
+    def update_solution(self, clicks):
+        outcome = self.comparison.infer_outcome(self.current_l,
+                                                self.current_context,
+                                                clicks,
+                                                self.current_query)
+        if outcome[0] > outcome[1]:
+            self.sampler.update_scores(self.r1, self.r2)
+        else:
+            self.sampler.update_scores(self.r2, self.r1)
+        self.iteration += 1
+        return self.get_solution()
