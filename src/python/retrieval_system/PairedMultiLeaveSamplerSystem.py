a2572b5e3dac07a74c57e5d4c0e3a7d20d49b713
Anne Schuth <anne.schuth@gmail.com>
2014-01-15 22:14:43 +0100

    bugfix, forgot per_q for paired

--- src/python/retrieval_system/PairedMultiLeaveSamplerSystem.py
+++ src/python/retrieval_system/PairedMultiLeaveSamplerSystem.py
@@ -12,13 +12,14 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with Lerot.  If not, see <http://www.gnu.org/licenses/>.
+from numpy import *
 
 from BaselineSamplerSystem import BaselineSamplerSystem
 
 
 class PairedMultiLeaveSamplerSystem(BaselineSamplerSystem):
     def get_ranked_list(self, query):
-        self.r1, self.r2, _, _ = self.sampler.get_arms()
+        self.r1, self.r2, self.i1, self.i2 = self.sampler.get_arms()
         (l, context) = self.comparison.interleave([self.r1, self.r2],
                                                   query,
                                                   self.nr_results)
@@ -32,9 +33,13 @@ class PairedMultiLeaveSamplerSystem(BaselineSamplerSystem):
                                                 self.current_context,
                                                 clicks,
                                                 self.current_query)
+        per_q = zeros([len(self.rankers), len(self.rankers)])
+
         if outcome[0] > outcome[1]:
             self.sampler.update_scores(self.r1, self.r2)
+            per_q[self.i1, self.i2] += 1
         else:
             self.sampler.update_scores(self.r2, self.r1)
+            per_q[self.i2, self.i1] += 1
         self.iteration += 1
-        return self.get_solution()
+        return self.get_solution(), per_q
