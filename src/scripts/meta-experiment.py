5dd246a7f936a5d20dbda7f922f82d0d4ee0bcf6
Anne Schuth <anne.schuth@uva.nl>
2014-01-16 11:28:43 +0100

    added configs

--- src/scripts/meta-experiment.py
+++ src/scripts/meta-experiment.py
@@ -15,10 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Lerot.  If not, see <http://www.gnu.org/licenses/>.
 
-try:
-    from include import *
-except:
-    pass
+from include import *
 from experiment import MetaExperiment
 
 if __name__ == "__main__":
