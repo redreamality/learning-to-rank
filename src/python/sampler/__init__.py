d84414cc71511f376e90820228a4572a0b4db7e8
Anne Schuth <anne.schuth@gmail.com>
2014-01-26 13:32:57 +0100

    implemented task2

--- src/python/sampler/__init__.py
+++ src/python/sampler/__init__.py
@@ -1,3 +1,4 @@
 #from RelativeThompsonSampler import RelativeThompsonSampler
 from BeatTheMeanSampler import BeatTheMeanSampler
 from BaselineSampler import BaselineSampler
+from BaselineSamplerTask2 import BaselineSamplerTask2
