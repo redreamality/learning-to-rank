074f3183371912d8a7252fcd358d8e08649a34f7
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-05-27 17:56:03 +0200

    kill off the useless function normalize_to_unit_sphere

--- lerot/utils/__init__.py
+++ lerot/utils/__init__.py
@@ -19,5 +19,4 @@ from .utils import get_class
 from .utils import split_arg_str
 from .utils import get_cosine_similarity
 from .utils import get_binomial_ci
-from .utils import normalize_to_unit_sphere
 from .utils import sample_unit_sphere
