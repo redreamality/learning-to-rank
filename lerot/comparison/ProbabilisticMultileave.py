6506bb4892a77c56f7d92b870a02e1b90d873be2
Jos van der Velde <jvvelde@gmail.com>
2015-01-13 15:04:22 +0100

    Added dummy-definitions where probabilities and credits can be implemented

--- lerot/comparison/ProbabilisticMultileave.py
+++ lerot/comparison/ProbabilisticMultileave.py
@@ -6,6 +6,7 @@ from numpy import asarray, e, log, where
 
 from AbstractInterleavedComparison import AbstractInterleavedComparison
 from ..utils import split_arg_str
+from nltk.test import probability_fixt
 
 
 class ProbabilisticMultileave(AbstractInterleavedComparison):
@@ -88,138 +89,54 @@ class ProbabilisticMultileave(AbstractInterleavedComparison):
                     pass
         return (asarray(l), a)
 
-    def infer_outcome(self, l, a, c, query):
+    def infer_outcome(self, l, rankers, c, query):
         '''
         TODO: DOCSTRING
 
         ARGS:
         - l: the created list of documents, using multileaving
-        - a: tuple containing td_a (team draft) and the rankers
-        - c: credits?
+        - rankers: the rankers
+        - c: the clicks
         - query: the query
 
         RETURNS
-        - The Credits (right ?)
+        - The Credits
         '''
-        (td_a, rankers) = a
 
-        # for comparisons with TD, use naive comparison
-        if self.compare_td:  # TODO: implement
-            raise Exception('Not implemented for multileave')
-            c1 = sum([1 if val_a == 0 and val_c == 1 else 0
-                for val_a, val_c in zip(td_a, c)])
-            c2 = sum([1 if val_a == 1 and val_c == 1 else 0
-                for val_a, val_c in zip(td_a, c)])
-            return -1 if c1 > c2 else 1 if c2 > c1 else 0
-
-        # comparison with marginalization
-        # are there any clicks? (otherwise it's a tie)
         click_ids = where(asarray(c) == 1)
         if not len(click_ids[0]):  # no clicks, will be a tie
             return 0
 
         rankers = [r.init_ranking(query) for r in rankers]
+        p = self.probability_of_list(l, rankers, click_ids)
+        credits = self.credits_of_list(p)
 
+        return credits
 
-        # enumerate all possible assignments that go with l, add their
-        # outcomes weighted by probabilities
-        # original outcome is not needed in this case, only clicks and probs
-        root = SimpleNAryTree(None, 0.0, 0)  # root
-        nextLevel = [root]
-        currentLevel = []
+    def probability_of_list(self, result_list, rankers, clickedDocs):
+        '''
+        ARGS:
+        - result_list: the multileaved list
+        - rankers: a list of rankers
+        - clickedDocs: the docIds in the result_list which recieved a click
 
-        # traverse possible assignments breath-first
-        log_p_a = len(l) * log(0.5)
-        log_p_l = len(l) * log(0.5)
-
-        for n in range(len(l)):
-            currentLevel = nextLevel
-            nextLevel = []
-            p_r1 = r1.get_document_probability(l[n])
-            p_r2 = r2.get_document_probability(l[n])
-            # zero probability: observed list is not possible (e.g., with
-            # deterministic rankers and historical data)
-            if p_r1 == 0 and p_r2 == 0:
-                return .0
-            r1.rm_document(l[n])
-            try:
-                r2.rm_document(l[n])
-            except:
-                pass
-            log_p_l += log(p_r1 + p_r2)
-
-            for node in currentLevel:
-                # expand children and add new nodes to queue for nextLevel
-                # left child: r1 is selected, only expand if p > 0
-                if p_r1 > 0:
-                    p_left = node.prob + log(0.5 * p_r1)
-                    o_left = node.outcome
-                    if c[n] == 1:
-                        o_left += -1
-                    node.left = SimpleNAryTree(node, p_left, o_left)
-                    nextLevel.append(node.left)
-                # right child: r2 is selected, only expand if p > 0
-                if p_r2 > 0:
-                    p_right = node.prob + log(0.5 * p_r2)
-                    o_right = node.outcome
-                    if c[n] == 1:
-                        o_right += 1
-                    node.right = SimpleNAryTree(node, p_right, o_right)
-                    nextLevel.append(node.right)
-        # we have all log probabilities for outcomes =! 0
-        o1 = 0.0
-        o2 = 0.0
-        for node in nextLevel:
-            if node.outcome != 0:
-                # log_p_a and log_p_l cancel out if we turn the outcome into a
-                # ratio for now, keep them for clarity
-                if node.outcome < 0:
-                    o1 += e ** (node.prob + log_p_a - log_p_l)
-                else:
-                    o2 += e ** (node.prob + log_p_a - log_p_l)
-
-        # return -1 if o1 > o2 else 1 if o2 > o1 else 0
-        if o1 == o2:
-            outcome = 0
-        elif self.aggregate == "expectation":
-            outcome = o2 - o1
-        elif self.aggregate == "log-likelihood-ratio":
-            if o1 > o2:
-                outcome = log(o2 / o1)
-            else:
-                outcome = log(o1 / o2)
-        elif self.aggregate == "likelihood-ratio":
-            if o1 > o2:
-                outcome = (float(o2) / o1) - 1
-            else:
-                outcome = 1 - (float(o1) / o2)
-        elif self.aggregate == "log-ratio":
-            if o1 > o2:
-                outcome = float(log(o1)) / log(o2) - 1
-            else:
-                outcome = 1 - float(log(o2)) / log(o1)
-        elif self.aggregate == "binary":
-            outcome = -1 if o1 > o2 else 1 if o2 > o1 else 0
-        else:
-            raise ValueError("Unknown aggregation method: %s", self.aggregate)
-        return outcome
-
-    def get_probability_of_list(self, result_list, context, query):
+        RETURNS
+        -p: list with the probability that the list comes from each ranker
+        '''
         # TODO: this is not implemented yet, only copied from Interleave
-        raise Exception("Not implemented")
+        p = None
+        return p
 
-        # P(l) = \prod_{doc in result_list} 1/2 P_1(doc) + 1/2 P_2(doc)
-        p_l = 1.0
-        (_, r1, r2) = context
-        r1.init_ranking(query)
-        r2.init_ranking(query)
-        for _, doc in enumerate(result_list):
-            p_r1 = r1.get_document_probability(doc)
-            p_r2 = r2.get_document_probability(doc)
-            r1.rm_document(doc)
-            r2.rm_document(doc)
-            p_l *= 0.5 * (p_r1 + p_r2)
-        return p_l
+    def credits_of_list(self, p):
+        '''
+        ARGS:
+        -p: list with the probability that the list comes from each ranker
+
+        RETURNS:
+        - credits: list of credits for each ranker
+        '''
+        credits = None
+        return credits
 
 
 class SimpleNAryTree:
