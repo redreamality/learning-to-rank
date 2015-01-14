87c7352ff725a9b2c89424aca2a9e6ff6e713eb3
Jos van der Velde <jvvelde@gmail.com>
2015-01-14 14:25:32 +0100

    Impressions: added teamDraft and interleaving, let it return preference matrices

--- lerot/comparison/test/script_ProbabilisticMultileave.py
+++ lerot/comparison/test/script_ProbabilisticMultileave.py
@@ -6,16 +6,18 @@ Created on 12 jan. 2015
 import cStringIO
 import random
 
+from lerot.comparison.ProbabilisticInterleave import ProbabilisticInterleave
 import lerot.comparison.ProbabilisticMultileave as ml
+from lerot.comparison.TeamDraftMultileave import TeamDraftMultileave
+import lerot.environment.CascadeUserModel as CascadeUserModel
 import lerot.query as qu
 import lerot.ranker.ProbabilisticRankingFunction as rnk
-import lerot.environment.CascadeUserModel as CascadeUserModel
 import numpy as np
 
 
-PATH_TEST_QUERIES = 'data/Fold1/test.txt'
-PATH_VALI_QUERIES = 'data/Fold1/vali.txt'
-PATH_TRAIN_QUERIES = 'data/Fold1/train.txt'
+PATH_TEST_QUERIES = '../../../data/Fold1/test.txt'
+PATH_VALI_QUERIES = '../../../data/Fold1/vali.txt'
+PATH_TRAIN_QUERIES = '../../../data/Fold1/train.txt'
 
 
 class Experiment(object):
@@ -39,6 +41,8 @@ class Experiment(object):
         query_fh.close()
 
         self.multil = ml.ProbabilisticMultileave()
+        self.interl = ProbabilisticInterleave('--aggregate binary')
+        self.TeamDraftMultileave = TeamDraftMultileave()
 
         self.rankers = [rnk("1", "random", self.n_features)
                    for _ in range(self.n_rankers)]
@@ -74,35 +78,80 @@ class Experiment(object):
         return total_creds / count
 
     def impression(self):
+        '''
+        TODO: docstring
+
+        RETURN:
+        - probabilistic multileaving: preference matrix
+        - team draft: preference matrix
+        - probabilisticInterleaving: tuple of (tuple with index of rankers),
+          credits)
+        '''
         # select query
         query = self.train_queries[random.choice(self.train_queries.keys())]
 
-        # probabilistic multileave
-        (ranking, _) = self.multil.multileave(self.rankers, query, self.k)
+        pm_creds = self.impression_probabilisticMultileave(query)
+        pi_creds, (pi_r1, pi_r2) = \
+            self.impression_probabilisticInterleave(query)
+        td_creds = self.impression_teamDraftMultileave(query)
+
+        pm_preferences = self.preferencesFromCredits(pm_creds)
+        td_preferences = self.preferencesFromCredits(td_creds)
+        print(pm_preferences)
+        print(td_preferences)
+        print(pi_creds)
+        return pm_preferences, td_preferences, ((pi_r1, pi_r2), pi_creds)
+
+    def impression_probabilisticMultileave(self, query):
+        ranking, _ = self.multil.multileave(self.rankers, query, self.k)
         clicks = self.user_model.get_clicks(ranking, query.get_labels())
-        pm_creds = self.multil.infer_outcome(ranking, self.rankers, clicks, query)
-
-        #probabilistic interleave
-        # pick pair of rankers
-        # interleave
-        # observe clicks
-        # assign credits
-
-        #team draft multileave
-        # multileave
-        # observe clicks
-        # assign credits
-
-        # merge all credits into preference matrix
-
-
+        creds = self.multil.infer_outcome(ranking, self.rankers, clicks,
+                                          query)
         return creds
+
+    def impression_probabilisticInterleave(self, query):
+        [r1, r2] = random.sample(self.rankers, 2)
+        ranking, _ = self.interl.interleave(r1, r2, query, self.k)
+        clicks = self.user_model.get_clicks(ranking, query.get_labels())
+        creds = self.interl.infer_outcome(ranking, (_, r1, r2),
+                                          clicks, query)
+        return creds, (self.rankers.index(r1), self.rankers.index(r2))
+
+    def impression_teamDraftMultileave(self, query):
+        ranking, a = self.TeamDraftMultileave.interleave(self.rankers, query,
+                                                         self.k)
+        clicks = self.user_model.get_clicks(ranking, query.get_labels())
+        creds = self.TeamDraftMultileave.infer_outcome(ranking, a, clicks,
+                                                          query)
+        return creds
+
+    def preferencesFromCredits(self, creds):
+        '''
+        ARGS:
+        - creds: a list with credits for each of the rankers
+
+        RETURNS:
+        - preferences: list of list containing 1 if ranker_row is better than
+          ranker_collumn, 0 if he is worse, 0.5 if he is equal
+        '''
+        n = len(self.rankers)
+        preferences = [[None] * n] * n
+        for i in range(len(self.rankers)):
+            for j in range(len(self.rankers)):
+                if creds[i] > creds[j]:
+                    pref = 1
+                elif creds[i] < creds[j]:
+                    pref = 0
+                else:
+                    pref = 0.5
+                preferences[i][j] = pref
+                preferences[j][i] = 1 - pref
+        return preferences
 
 
 def _readQueries(path):
     with open(path, "r") as myfile:
         data = myfile.read()
-    data = '\n'.join(data.splitlines())
     return data
 
 
